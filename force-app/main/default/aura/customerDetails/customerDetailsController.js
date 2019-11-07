3601af2d64b4d63d5e6643cea0864c2cb486b177
kennethkim1221 <kennethkim1221@gmail.com>
2019-11-07 11:41:12 +0800

    updated comment

--- force-app/main/default/aura/customerDetails/customerDetailsController.js
+++ force-app/main/default/aura/customerDetails/customerDetailsController.js
@@ -1,6 +1,7 @@
 ({
     draftReservation : function(component, event, helper)
     {
+        // Question after these are set what will happen to them? since we will advance to the next part of the flow?
         component.set("v.startDate", event.getParam("startDate"));
         component.set("v.endDays", event.getParam("endDays"));
         component.set("v.numberOfPeople", event.getParam("numberOfPeople"));
